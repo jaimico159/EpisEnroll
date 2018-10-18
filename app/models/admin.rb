b7f975d9970f1add7cb1c71a74fd1575d256441e
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-10-18 12:58:13 -0500

    Creates Admin Model with roles

---
+++ app/models/admin.rb
@@ -0,0 +1,8 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum user_role: { admin: 0, director: 1, secretary: 2, administrative: 3 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_174427) do
+ActiveRecord::Schema.define(version: 2018_10_18_174916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "first_name", limit: 45, default: "", null: false
+    t.string "last_name", limit: 45, default: "", null: false
+    t.integer "admin_role", limit: 2, default: 3, null: false
+    t.text "description", default: ""
+    t.string "status", default: "A", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "clasifications", force: :cascade do |t|
     t.integer "value", limit: 2, null: false
