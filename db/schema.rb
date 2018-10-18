c8121c14bd15201808db59c6b8c7c88e0efc54a4
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-10-18 11:42:14 -0500

    Creates Teacher model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_160730) do
+ActiveRecord::Schema.define(version: 2018_10_18_163637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,22 @@ ActiveRecord::Schema.define(version: 2018_10_18_160730) do
     t.index ["cui"], name: "index_students_on_cui", unique: true
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "first_name", limit: 45, default: "", null: false
+    t.string "last_name", limit: 45, default: "", null: false
+    t.text "description", default: ""
+    t.string "status", default: "A", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_teachers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
   add_foreign_key "students", "clasifications"
