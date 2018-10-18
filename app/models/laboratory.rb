b991cd449a8a69c85adbd1746df62de6ab356f6a
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-10-18 12:15:55 -0500

    Adds course to Laboratory

--- app/models/laboratory.rb
+++ app/models/laboratory.rb
@@ -1,2 +1,3 @@
 class Laboratory < ApplicationRecord
+    belongs_to :course
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_170922) do
+ActiveRecord::Schema.define(version: 2018_10_18_171349) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,8 @@ ActiveRecord::Schema.define(version: 2018_10_18_170922) do
     t.string "status", default: "A", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "course_id"
+    t.index ["course_id"], name: "index_laboratories_on_course_id"
   end
 
   create_table "semesters", force: :cascade do |t|
@@ -99,5 +101,6 @@ ActiveRecord::Schema.define(version: 2018_10_18_170922) do
     t.index ["reset_password_token"], name: "index_teachers_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "laboratories", "courses"
   add_foreign_key "students", "clasifications"
 end
