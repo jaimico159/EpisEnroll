eb0b6f21f0eadb4bc24a8db8e28f20349fc3ffdf
Gabriel Alex Justo Huayna <gjusto@unsa.edu.pe>
2018-10-21 01:13:38 -0500

    configurando el modelo Estudentpara el inicio de session, creando con datos de prueba, luego se puede optimizar

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,21 @@ class Student < ApplicationRecord
   #devise :database_authenticatable, :registerable,
   #       :recoverable, :rememberable, :validatable
   enum status: { inactive: 0, active: 1}
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.course_codes = [1,2,54]
+      user.first_name = auth.info.name
+      user.email = auth.info.email
+      user.encrypted_password = "xdxdxdxd"
+      user.password = "sdsd1224"
+      user.last_name = "perez"
+      user.cui = 20160662
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.clasification_id = 2
+       user.save!
+    end
+  end
 end
