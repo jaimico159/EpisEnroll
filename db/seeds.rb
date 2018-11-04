49146061b618f53bb451f4edc3ef06ec9d3493ee
max <maxalcubo@github.com>
2018-11-03 22:21:31 -0500

    arregle-un-error-de-escritura-en-el-archivo-seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Clasifications.delete_all
+Clasification.delete_all
 
-Clasifications.create(value: 1, name: "Primer tercio", start_date: Date.today, finish_date: Date.today + 5, description: "", status: :active)
-Clasifications.create(value: 2, name: "Segundo tercio", start_date: Date.today, finish_date: Date.today + 5, description: "", status: :active)
-Clasifications.create(value: 3, name: "Tercio tercio", start_date: Date.today, finish_date: Date.today + 5, description: "", status: :active)
-Clasifications.create(value: 4, name: "Cachimbos", start_date: Date.today, finish_date: Date.today + 5, description: "", status: :active)
+Clasification.create(value: 1, name: "Primer tercio", start_date: Date.today, finish_date: Date.today + 5, description: "", status: "1")
+Clasification.create(value: 2, name: "Segundo tercio", start_date: Date.today, finish_date: Date.today + 5, description: "", status: "1")
+Clasification.create(value: 3, name: "Tercio tercio", start_date: Date.today, finish_date: Date.today + 5, description: "", status: "1")
+Clasification.create(value: 4, name: "Cachimbos", start_date: Date.today, finish_date: Date.today + 5, description: "", status: "1")
 
-Groups.delete_all
+Group.delete_all
 
-Groups.create(name: "A", description: "", status: :active)
-Groups.create(name: "B", description: "", status: :active)
-Groups.create(name: "C", description: "", status: :active)
-Groups.create(name: "D", description: "", status: :active)
-Groups.create(name: "E", description: "", status: :active)
+Group.create(name: "A", description: "", status: "1")
+Group.create(name: "B", description: "", status: "1")
+Group.create(name: "C", description: "", status: "1")
+Group.create(name: "D", description: "", status: "1")
+Group.create(name: "E", description: "", status: "1")
