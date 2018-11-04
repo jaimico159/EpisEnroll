cc13a5353d4b25bea6069c936c9e3936b873a0ce
Gerald <saoksgerald9@gmail.com>
2018-11-04 12:04:18 -0500

    modify_models(full_name)_and_router

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :students
 
   # Importante para usar las rutas de rails debes crear los siguiente: resources :'tumodelo'
+  resources :admins
   resources :students
 
   get 'auth/:provider/callback', to: 'sessions#google_oauth2'
