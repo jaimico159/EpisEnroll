8015a485ea920d616f185d84d606765c4b697034
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-11-05 06:44:21 -0500

    Fix teachers variable problem

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,9 +1,9 @@
 class TeachersController < ApplicationController
-  before_action :authenticate_teacher!
+  #before_action :authenticate_teacher!
   before_action :set_user, only: %i[show edit update destroy]
 
 	def index
-		@teacher = Teacher.all
+		@teachers = Teacher.all
 	end
 
 	def new
