829b8aed4c2222ac38163fe2eda48c4521668b84
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-11-24 11:10:11 -0500

    change counter name for using cache counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_231528) do
+ActiveRecord::Schema.define(version: 2018_11_24_150952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2018_11_22_231528) do
   end
 
   create_table "enrollment_headers", force: :cascade do |t|
-    t.integer "laboratory_counter", limit: 2, default: 0, null: false
+    t.integer "enrollment_details_count", limit: 2, default: 0, null: false
     t.text "description", default: ""
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
