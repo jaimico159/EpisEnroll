c754d7982fb7ef3f1fe8bc9a3556b4ce8a479ddf
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-11-24 11:14:29 -0500

    adds counter cache for enrollment details

--- app/models/enrollment_detail.rb
+++ app/models/enrollment_detail.rb
@@ -1,5 +1,5 @@
 class EnrollmentDetail < ApplicationRecord
-  belongs_to :enrollment_header
+  belongs_to :enrollment_header, counter_cache: true
   belongs_to :laboratory, :foreign_key => [:course_id, :group_id]
   enum status: { inactive: 0, active: 1}
 end
