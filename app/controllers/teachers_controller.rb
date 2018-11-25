481e4456fd3ae00f885a12d3944b6cbec1f958f8
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-11-25 18:04:21 -0500

    Teachers views using pundit authorization

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,6 +1,6 @@
 class TeachersController < ApplicationController
-	before_action :authenticate_teacher!, only: %i[home]
-	before_action :authenticate_admin!, only: %i[new edit show update destroy]
+	before_action :authenticate_teacher!, only: %i[home my_labs]
+	before_action :authenticate_admin!, except: %i[home my_labs]
   before_action :set_user, only: %i[show edit update destroy]
 
   def home
@@ -8,15 +8,17 @@ class TeachersController < ApplicationController
   end
   
   def my_labs
-	@laboratories = current_teacher.laboratories
-	@teacher = current_teacher
+	  @laboratories = current_teacher.laboratories
+	  @teacher = current_teacher
   end
 
-	def index
+  def index
+    authorize current_admin, policy_class: TeacherPolicy
 		@teachers = Teacher.all
 	end
 
-	def new
+  def new
+    authorize current_admin, policy_class: TeacherPolicy
 		@teacher = Teacher.new
 	end
 
@@ -26,46 +28,53 @@ class TeachersController < ApplicationController
 	def edit
 	end
 	
-	def create
-    	@teacher = Teacher.new(teacher_params)
-     	respond_to do |format|
-    	  	if @teacher.save
-        		format.html { redirect_to @teacher, notice: 'Teacher was succesfully created' }
-        		format.json { render :show, status: :created, location: @teacher }
-      		else
-        		format.html { render :new }
-        		format.json { render json: @teacher.errors, status: :unprocessable_entity }
-      		end
-    	end
-  	end
+  def create
+    authorize current_admin, policy_class: TeacherPolicy
+    @teacher = Teacher.new(teacher_params)
+    respond_to do |format|
+      if @teacher.save
+        format.html { redirect_to @teacher, notice: 'Teacher was succesfully created' }
+        format.json { render :show, status: :created, location: @teacher }
+      else
+        format.html { render :new }
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
 	def update
 		respond_to do |format|
-     		if @teacher.update(teacher_params)
-        		format.html { redirect_to @teacher, notice: 'Teacher was successfully updated.' }
-        		format.json { render :show, status: :ok, location: @Teacher }
-      		else
-        		format.html { render :edit }
-       			format.json { render json: @teacher.errors, status: :unprocessable_entity }
-      		end
-    	end
+      if @teacher.update(teacher_params)
+        format.html { redirect_to @teacher, notice: 'Teacher was successfully updated.' }
+        format.json { render :show, status: :ok, location: @Teacher }
+      else
+        format.html { render :edit }
+        format.json { render json: @teacher.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	def destroy
 		@teacher.destroy
-    	respond_to do |format|
-      		format.html { redirect_to teacher_url, notice: 'Teacher was successfully destroyed.' }
-      		format.json { head :no_content }
-    	end
+    respond_to do |format|
+      format.html { redirect_to teacher_url, notice: 'Teacher was successfully destroyed.' }
+      format.json { head :no_content }
+    end
 	end
 
 	private
+	
+	def user_not_authorized
+		flash[:alert] = "You are not authorized to perform this action."
+		redirect_to(request.referrer || root_path)
+	end
 
-	def set_user
-    	@teacher = Teacher.find(params[:id])
+  def set_user
+    authorize current_admin, policy_class: TeacherPolicy
+    @teacher = Teacher.find(params[:id])
  	end
 
-  	def teacher_params
-    	params.fetch(:teacher, {}).permit(:first_name, :last_name, :email, :description, :status)
-  	end
+  def teacher_params
+    params.fetch(:teacher, {}).permit(:first_name, :last_name, :email, :description, :status)
+  end
 end
--- app/policies/teacher_policy.rb
+++ app/policies/teacher_policy.rb
@@ -1,61 +1,65 @@
-class CoursesPolicy
-    attr_reader :user, :record
-  
-    def initialize(user, record)
-      @user = user
-      @record = record
-    end
-  
-    def index?
-      true
-    end
-  
-    def new?
-      if user.is_a?(Admin)
-        user.admin? || user.director? || user.secretary?
-      else
-        false
-      end
-    end
-  
-    def edit?
-      if user.is_a?(Admin)
-        user.admin? || user.director? || user.secretary?
-      else
-        false
-      end
-    end
-  
-    def show?
-      if user.is_a?(Admin)
-        true
-      else
-        false
-      end
-    end
-  
-    def update?
-      if user.is_a?(Admin)
-        user.admin? || user.director? || user.secretary?
-      else
-        false
-      end
-    end
-  
-    def destroy?
-      if user.is_a?(Admin)
-        user.admin? || user.director? || user.secretary?
-      else
-        false
-      end
-    end
-  
-    def create?
-      if user.is_a?(Admin)
-        user.admin? || user.director? || user.secretary?
-      else
-        false
-      end
-    end
-    
+class TeacherPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
   end
+
+  def index?
+    if user.is_a?(Admin)
+      true
+    else
+      false
+    end
+  end
+
+  def new?
+    if user.is_a?(Admin)
+      user.admin? || user.director? || user.secretary?
+    else
+      false
+    end
+  end
+
+  def edit?
+    if user.is_a?(Admin)
+      user.admin? || user.director? || user.secretary?
+    else
+      false
+    end
+  end
+
+  def show?
+    if user.is_a?(Admin)
+      true
+    else
+      false
+    end
+  end
+
+  def update?
+    if user.is_a?(Admin)
+      user.admin? || user.director? || user.secretary?
+    else
+      false
+    end
+  end
+
+  def destroy?
+    if user.is_a?(Admin)
+      user.admin? || user.director? || user.secretary?
+    else
+      false
+    end
+  end
+
+  def create?
+    if user.is_a?(Admin)
+      user.admin? || user.director? || user.secretary?
+    else
+      false
+    end
+  end
+  
+end
