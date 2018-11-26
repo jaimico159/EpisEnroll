af2ff5874df3da0dfddcf2b08158bc6493a5fd07
JavierApaza7991 <japazah@unsa.edu.pe>
2018-11-25 23:31:02 -0500

    Mejora de la actulización del select_group dependiendo de select_course en formulario de laboratorio.

--- app/assets/javascripts/laboratory.js
+++ app/assets/javascripts/laboratory.js
@@ -1,30 +1,29 @@
 $(document).ready(function() {
     $('select#laboratory_course_id').select2();
     $('select#laboratory_group_id').select2();
-    
-    $("#laboratory_course_id").on('change', function() {
-        var course_id = $("#laboratory_course_id").val();
-        var groups = [];
-        $.getJSON("/courses/" + course_id + "/unused_groups.json", (data) => {
-            groups = data.map((group, i) => {
-                return {id: group.id, name: group.name};
-            });
-            let input = $("#laboratory_group_id");
-            console.log(groups);
-            input.select2({
-               data: groups,
-            });
-
-            var observer = new MutationObserver(function (mutations) {
-                if (mutations[0].type === 'chilList') {
-                    $("#share").prop('disabled', false);
-                }
-            });
-
-            var observerConfig = {
-                childList: true,
-                characterData: true
-            };
-        });
-    });
 });
+
+function update_select_group() {
+    var course_id = $("#laboratory_course_id").val();
+    var groups = [];
+    $.getJSON("/courses/" + course_id + "/unused_groups.json", (data) => {
+        groups = data.map((group, i) => {
+            return {id: group.id, name: group.name};
+        });
+        document.getElementById("laboratory_group_id").innerHTML = "";
+        for(var i in groups) {
+            document.getElementById("laboratory_group_id").innerHTML += "<option value='"+groups[i].id+"'>"+groups[i].name+"</option>"; 
+        }
+
+        var observer = new MutationObserver(function (mutations) {
+            if (mutations[0].type === 'chilList') {
+                $("#share").prop('disabled', false);
+            }
+        });
+
+        var observerConfig = {
+            childList: true,
+            characterData: true
+        };
+    });
+}
