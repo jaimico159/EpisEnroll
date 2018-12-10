8193b3a622f3d3435e94bd0a049b258f1445e257
Gary Nuñez <gnunezc@unsa.edu.pe>
2018-12-10 00:06:12 -0500

    Assertions for all the controllers

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe StudentsController, type: :controller do
 
+  describe "GET index" do
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
+
   describe "GET #index" do
     it "returns http success" do
       get :index
@@ -27,6 +34,34 @@ RSpec.describe StudentsController, type: :controller do
     it "returns http success" do
       get :edit
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #index" do
+    it "returns http failed" do
+      get :index
+      expect(response).not_to have_http_status(:success)
+    end
+  end
+
+  describe "GET #new" do
+    it "returns http failed" do
+      get :new
+      expect(response).not_to have_http_status(:success)
+    end
+  end
+
+  describe "GET #show" do
+    it "returns http failed" do
+      get :show
+      expect(response).not_to have_http_status(:success)
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http failed" do
+      get :edit
+      expect(response).not_to have_http_status(:success)
     end
   end
 
