9b62f1cb5119031242aee5e46dbd3797f3ef0286
Gary Nuñez <gnunezc@unsa.edu.pe>
2018-12-10 00:32:34 -0500

    Creating random data with the gem factory bot

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -2,4 +2,15 @@ FactoryBot.define do
   factory :teacher do
     
   end
+
+  factory :random_teacher, class: Teacher do
+	first_name {Faker::Name.first_name}
+	last_name {Faker::Name.last_name}
+	description {Faker::Lorem.sentence}
+	email {Faker::Internet.email}
+	encrypted_password {Faker::Internet.password}
+	status: :active
+  end
+
+
 end
