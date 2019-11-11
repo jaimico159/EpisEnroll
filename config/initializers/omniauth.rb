b1c777e0eb551233f4d044a1ae1e3e1f68dc1350
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2019-11-11 16:19:04 -0500

    removes omniauth google oauth2

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-OmniAuth.config.logger = Rails.logger
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_OAUTH_CLIENT_ID'], ENV['GOOGLE_OAUTH_CLIENT_SECRET'], skip_jwt: true
-#,{client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
-
-
-end													 
